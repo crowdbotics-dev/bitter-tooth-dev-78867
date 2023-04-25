229b438d05b634e819c4401e2095359a3569cd6d
Crowdbotics <engineering@crowdbotics.com>
2023-04-25 07:40:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bitter-tooth-dev-78867",
+  url: "https://bitter-tooth-dev-78867.botics.co",
+  api: "https://bitter-tooth-dev-78867.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
